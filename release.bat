c103a3a09997d024421b9e5fbfc099f93c2386d0
AlphonseLuca <alphonseluca@163.com>
2024-11-29 21:48:14 +0800

    add icon

---
+++ release.bat
@@ -0,0 +1,5 @@
+@echo off
+
+xcopy /E /I /Y src "D:\G_Games\steam\steamapps\common\Don't Starve Together\mods\Tea-of-Pig"
+
+echo Files copied successfully.
---
+++ src/modinfo.lua
@@ -0,0 +1,24 @@
+-- 名称
+name = "Tea of Pig"
+-- 描述
+description = "Sample Mod"
+-- 作者
+author = "Alphosne"
+-- 版本
+version = "0.1"
+-- klei官方论坛地址，为空则默认是工坊的地址
+forumthread = ""
+-- modicon
+icon_atlas = "modicon.xml"
+icon = "modicon.tex"
+-- dst兼容
+dst_compatible = true
+-- 是否是客户端mod
+client_only_mod = true
+-- 是否是所有客户端都需要安装
+all_clients_require_mod = false
+-- 饥荒api版本，固定填10
+api_version = 10
+
+-- mod的配置项，后面介绍
+configuration_options = {}
